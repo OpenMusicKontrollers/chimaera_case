5fb3d56b140dd980458cafc73b9e96e77568d78e
Hanspeter Portner <agenthp@users.sf.net>
2013-12-06 11:42:12 +0100

    replaced zlib/libpng license with CERN OHL v.1.2

--- library/chimaera.js
+++ library/chimaera.js
@@ -1,24 +1,11 @@
 /*
- * Copyright (c) 2013 Hanspeter Portner (dev@open-music-kontrollers.ch)
- * 
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * 
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 
- *     1. The origin of this software must not be misrepresented; you must not
- *     claim that you wrote the original software. If you use this software
- *     in a product, an acknowledgment in the product documentation would be
- *     appreciated but is not required.
- * 
- *     2. Altered source versions must be plainly marked as such, and must not be
- *     misrepresented as being the original software.
- * 
- *     3. This notice may not be removed or altered from any source
- *     distribution.
+ * This documentation describes Open Hardware and is licensed under the
+ * CERN OHL v.1.2. You may redistribute and modify this documentation
+ * under the terms of the CERN OHL v.1.2. (http://ohwr.org/cernohl). This
+ * documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,
+ * INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE. Please see the CERN OHL v.1.2 for applicable
+ * conditions.
  */
 
 include("./wrapping.js");
